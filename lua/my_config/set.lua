cb022fda46aebfc844369475cfb11ae8d882d28e
fudido <fudido>
2025-11-23 15:24:47 -0300

    fix

--- lua/my_config/set.lua
+++ lua/my_config/set.lua
@@ -1,6 +1,8 @@
-vim.opt.tabstop = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" 
-vim.opt.undofile = true 
+vim.opt.undofile = true
+vim.opt.tabstop = 4       -- tamanho do tab no arquivo
+vim.opt.shiftwidth = 4    -- tamanho da indentação ao dar >> ou autoindent
+vim.opt.expandtab = true  -- usa espaços em vez de tabs
---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,62 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,  -- carrega no startup
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+⠀⠀⠀⠀⠀⠀⠀⣠⡤⠶⡄⠀⠀⠀⠀⠀⠀⠀⢠⠶⣦⣀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣴⣿⡟⠀⠈⣀⣾⣝⣯⣿⣛⣷⣦⡀⠀⠈⢿⣿⣦⡀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⣿⡇⠀⢼⣿⣽⣿⢻⣿⣻⣿⣟⣷⡄⠀⢸⣿⣿⣾⣄⠀⠀⠀
+⠀⠀⣞⣿⣿⣿⣿⣷⣤⣸⣟⣿⣿⣻⣯⣿⣿⣿⣿⣀⣴⣿⣿⣿⣿⣯⣆⠀⠀
+⠀⡼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣜⡆⠀
+⢠⣟⣯⣿⣿⣿⣷⢿⣫⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣟⠿⣿⣿⣿⣿⡷⣾⠀
+⢸⣯⣿⣿⡏⠙⡇⣾⣟⣿⡿⢿⣿⣿⣿⣿⣿⢿⣟⡿⣿⠀⡟⠉⢹⣿⣿⢿⡄
+⢸⣯⡿⢿⠀⠀⠱⢈⣿⢿⣿⡿⣏⣿⣿⣿⣿⣿⣿⣿⣿⣀⠃⠀⢸⡿⣿⣿⡇
+⢸⣿⣇⠈⢃⣴⠟⠛⢉⣸⣇⣹⣿⣿⠚⡿⣿⣉⣿⠃⠈⠙⢻⡄⠎⠀⣿⡷⠃
+⠈⡇⣿⠀⠀⠻⣤⠠⣿⠉⢻⡟⢷⣝⣷⠉⣿⢿⡻⣃⢀⢤⢀⡏⠀⢠⡏⡼⠀
+⠀⠘⠘⡅⠀⣔⠚⢀⣉⣻⡾⢡⡾⣻⣧⡾⢃⣈⣳⢧⡘⠤⠞⠁⠀⡼⠁⠀⠀
+⠀⠀⠀⠸⡀⠀⢠⡎⣝⠉⢰⠾⠿⢯⡘⢧⡧⠄⠀⡄⢻⠀⠀⠀⢰⠁⠀⠀⠀
+⠀⠀⠀⠀⠁⠀⠈⢧⣈⠀⠘⢦⠀⣀⠇⣼⠃⠰⣄⣡⠞⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢤⠼⠁⠀⠀⠳⣤⡼⠀⠀⠀⠀⠀⠀
+    ]], 
+        -- botões interativos
+        keys = {
+          {
+            key = "e",
+            desc = "  New file",
+            action = function()
+              vim.cmd("ene | startinsert")  -- cria um arquivo novo
+            end,
+          },
+          {
+            key = "f",
+            desc = "  Find file",
+            action = function()
+              require("snacks").picker.files()  -- abre o picker de arquivos
+            end,
+          },
+          {
+            key = "q",
+            desc = "  Quit",
+            action = function()
+              vim.cmd("qa")
+            end,
+          },
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+    -- abre a dashboard automaticamente ao iniciar o Neovim
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        require("snacks").dashboard()
+      end,
+    })
+  end,
+}
+
