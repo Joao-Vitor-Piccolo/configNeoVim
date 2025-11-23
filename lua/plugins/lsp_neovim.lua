c6cd8ae109d00966e906060101bf5a5ff86ee3f7
fudido <fudido>
2025-11-23 19:20:00 -0300

    fix

--- lua/plugins/lsp_neovim.lua
+++ lua/plugins/lsp_neovim.lua
@@ -1,8 +1,26 @@
+-- ~/.config/nvim/lua/plugins/lsp_neovim.lua
 return {
-  "neovim/nvim-lspconfig",
-  version = "v1.24.0", -- versão compatível com NVIM 0.9
-  config = function()
-    -- sua configuração de LSP
-  end
+  {
+    "neovim/nvim-lspconfig",
+    version = false,
+    config = function()
+      local ok, lspconfig = pcall(require, "lspconfig")
+      if not ok then
+        vim.notify("nvim-lspconfig não carregou", vim.log.levels.ERROR)
+        return
+      end
+
+      -- TS / TSX
+      lspconfig.ts_ls.setup({
+        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+        capabilities = vim.lsp.protocol.make_client_capabilities(),
+        on_attach = function(client, bufnr)
+          local opts = { noremap = true, silent = true }
+          vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        end,
+      })
+    end,
+  },
 }
 
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,8 @@
+return {
+  -- LSP config
+  {
+    "neovim/nvim-lspconfig",
+    version = false,  -- pega sempre a última versão
+  },
+}
+
