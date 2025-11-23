8d7556d68b69b21f253a7ffd18ca3a25ce885c37
fudido <fudido>
2025-11-23 15:53:22 -0300

    fix

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,12 +1,11 @@
-return {
+return-- lazy.nvim
+{
   "folke/snacks.nvim",
-  lazy = false,  -- carrega no startup
   ---@type snacks.Config
   opts = {
     dashboard = {
-      enabled = true,
-      preset = {
-        header = [[
+        preset = {
+            header = [[
 ⠀⠀⠀⠀⠀⠀⠀⣠⡤⠶⡄⠀⠀⠀⠀⠀⠀⠀⢠⠶⣦⣀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⢀⣴⣿⡟⠀⠈⣀⣾⣝⣯⣿⣛⣷⣦⡀⠀⠈⢿⣿⣦⡀⠀⠀⠀⠀
 ⠀⠀⠀⣴⣿⣿⣿⡇⠀⢼⣿⣽⣿⢻⣿⣻⣿⣟⣷⡄⠀⢸⣿⣿⣾⣄⠀⠀⠀
@@ -21,42 +20,9 @@ return {
 ⠀⠀⠀⠸⡀⠀⢠⡎⣝⠉⢰⠾⠿⢯⡘⢧⡧⠄⠀⡄⢻⠀⠀⠀⢰⠁⠀⠀⠀
 ⠀⠀⠀⠀⠁⠀⠈⢧⣈⠀⠘⢦⠀⣀⠇⣼⠃⠰⣄⣡⠞⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢤⠼⠁⠀⠀⠳⣤⡼⠀⠀⠀⠀⠀⠀
-    ]], 
-        -- botões interativos
-        keys = {
-          {
-            key = "e",
-            desc = "  New file",
-            action = function()
-              vim.cmd("ene | startinsert")  -- cria um arquivo novo
-            end,
-          },
-          {
-            key = "f",
-            desc = "  Find file",
-            action = function()
-              require("snacks").picker.files()  -- abre o picker de arquivos
-            end,
-          },
-          {
-            key = "q",
-            desc = "  Quit",
-            action = function()
-              vim.cmd("qa")
-            end,
-          },
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    require("snacks").setup(opts)
-    -- abre a dashboard automaticamente ao iniciar o Neovim
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        require("snacks").dashboard()
-      end,
-    })
-  end,
+                
+            ]]
+                }
+    }
+  }
 }
-
