b16ca6b33c471ae5f0d1624dddd84465cc5b1daa
Joao Vitor Piccolo <joaovitorpiccolo015@gmail.com>
2026-06-25 15:23:26 -0300

    style: colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,24 @@
 return {
-  "bluz71/vim-moonfly-colors",
-  lazy = false,
-  transparecy = true,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme moonfly")
-    vim.cmd([[
-    highlight Normal guibg=NONE ctermbg=NONE
-    highlight EndOfBuffer guibg=NONE
-    ]])
-  end,
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        -- Estilos disponíveis: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
+        style = 'darker',
+        transparent = true, -- Altere para true se quiser transparência
+        term_colors = true,
+        ending_tildes = false,
+        cmp_itemkind_reverse = false,
+        code_style = {
+            comments = 'italic',
+            keywords = 'italic',
+            functions = 'none',
+            strings = 'none',
+            variables = 'none'
+        },
+    },
+    config = function(_, opts)
+        require('onedark').setup(opts)
+        vim.cmd('colorscheme onedark')
+    end,
 }
